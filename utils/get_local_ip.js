03c04d49f08a9b6142340a5659000e81581b86fd
Chris Carlson <Chris@cta.cloud>
2017-03-14 07:07:30 -0400

    Initial Commit

---
+++ utils/get_local_ip.js
@@ -0,0 +1,29 @@
+
+// GLIP: Get Local IP
+glip = function glip() {
+    let pr = new Promise(
+        (resolve, reject) => {
+            ip.v4().then(
+                function (ip) {
+                    if (ip) {
+                        resolve(ip)
+                    } else {
+                        reject(reason);
+                    }
+                }
+            );
+        }
+    ).
+    then(
+        function (val) {
+            console.log("Promise resolved.");
+            console.log(`IP_ADDR: ${val}`);
+            return true;
+        }
+    ).
+    catch(
+        function (reason) {
+            console.log(`Promise Rejected!: ${reason}`);
+        }
+    );
+}
